91947ea4bb177645164b9ebcd93b7d14ef7fc289
Az3003 <az3003@163.com>
2022-01-11 21:32:57 +0800

    2022.1.11 test2

--- cloudali-sentinel-service8401/src/main/java/cn/az/springcloud/alibaba/controller/TestString.java
+++ cloudali-sentinel-service8401/src/main/java/cn/az/springcloud/alibaba/controller/TestString.java
@@ -3,6 +3,8 @@ package cn.az.springcloud.alibaba.controller;
 public class TestString {
 
     public static void main(String[] args) {
+        String s = "ne";
+        System.out.println(s);
     }
 
 }
